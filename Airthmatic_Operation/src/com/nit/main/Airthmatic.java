53f1e864a159054206765b7999b0583b56a51011
shekh <shekh@DESKTOP-47MBRH3>
2025-04-02 08:24:05 +0530

    commit

---
+++ Airthmatic_Operation/src/com/nit/main/Airthmatic.java
@@ -0,0 +1,14 @@
+package com.nit.main;
+
+public class Airthmatic {
+
+	public static void main(String[] args) {
+		
+		int a = 30;
+		int b = 20;
+		int sum = a + b;
+		System.out.println("A + b are Sum is :: "+sum);
+
+	}
+
+}
---
+++ Airthmatic_Operation/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Airthmatic_Operation {
+}
